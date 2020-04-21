98826414ffc9b3cc4eb89e13d00707ddead4f9e2
ParkDajeong <sobeast980@gmail.com>
2020-04-21 14:30:27 +0900

    자바스크립트 계산기 마크업 작업 완료

---
+++ Vanilla JS Calculator/calculator.js
@@ -0,0 +1,6 @@
+/*
+    구글 '계산기' 이미지 검색
+    구글 'js calculator' 이미지 검색
+    https://codepen.io/zhann1982/pen/YZeLwq
+    https://fonts.google.com/?preview.text=AC+EC+7+8+9+4+5+6+1+2+3+%2B+%2F+-+%25&preview.text_type=custom&sidebar.open
+*/
