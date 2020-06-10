ef0979646ad94782c64e75fda9c2c69902f0f517
ParkDajeong <sobeast980@gmail.com>
2020-06-10 16:52:38 +0900

    [Image Sliders] 이미지 슬라이더 연습-01

---
+++ Pure JS/Image Slide/ImgSlide.js
@@ -0,0 +1,52 @@
+"use strict!";
+const pageBtn = document.querySelectorAll(".first .dot");
+
+function moveSlide01() {
+  const imgList = document.querySelector(".first .imgList");
+  const width = imgList.offsetWidth;
+  const idx = this.dataset.slide;
+
+  pageBtn.forEach((el) => el.classList.remove("active"));
+  this.classList.add("active");
+  imgList.style.transform = `translateX(-${width * idx}px)`;
+}
+
+pageBtn.forEach((el) => el.addEventListener("click", moveSlide01));
+pageBtn[0].click();
+
+///////////////////////////////////////////////////////////////////////
+const arrowBtns = document.querySelectorAll(".second .btn");
+const imgList = document.querySelector(".second .imgList");
+
+const firstClone = imgList.firstElementChild.cloneNode(true);
+const lastClone = imgList.lastElementChild.cloneNode(true);
+imgList.prepend(lastClone);
+imgList.append(firstClone);
+
+const width = imgList.firstElementChild.offsetWidth;
+imgList.style.transform = `translateX(-${width}px)`;
+
+let curIndex = 1;
+
+function moveSlide02() {
+  const move = parseInt(this.dataset.move);
+  const imgs = imgList.querySelectorAll("li");
+
+  curIndex += move;
+  imgList.style.transform = `translateX(-${width * curIndex}px)`;
+  if (curIndex >= imgs.length - 1) {
+    curIndex = 1;
+    setTimeout(() => {
+      imgList.style.transition = "all 0s";
+      imgList.style.transform = `translateX(-${width * curIndex}px)`;
+    }, 10);
+  } else if (curIndex <= 0) {
+    curIndex = imgs.length - 2;
+    setTimeout(() => {
+      imgList.style.transition = "all 0s";
+      imgList.style.transform = `translateX(-${width * curIndex}px)`;
+    }, 10);
+  }
+}
+
+arrowBtns.forEach((el) => el.addEventListener("click", moveSlide02));
