0b225b812924d1eefddbe413ec3f417ec60d6feb
ParkDajeong <sobeast980@gmail.com>
2020-07-09 10:53:56 +0900

    [DreamCoding] 좌표 실습 성능 개선(top, left -> transform: translate 변경)

--- DreamCoding/browser/ch01/coordinates/main.js
+++ DreamCoding/browser/ch01/coordinates/main.js
@@ -1,21 +1,25 @@
 const coordinates = document.querySelector(".coordinates");
 const horozontal = document.querySelector(".horozontal");
 const vertical = document.querySelector(".vertical");
+const target = document.querySelector(".target");
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
+
+console.log(targetRect);
+console.log(targetHalfWidth, targetHalfHeight);
 
 function moveImgage(e) {
-  const img = document.querySelector("img");
   const mouseX = e.clientX;
   const mouseY = e.clientY;
 
-  img.style.left = `${mouseX}px`;
-  img.style.top = `${mouseY}px`;
+  horozontal.style.transform = `translateY(${mouseY}px)`;
+  vertical.style.transform = `translateX(${mouseX}px)`;
 
+  target.style.transform = `translate(${mouseX - targetHalfWidth}px, ${mouseY - targetHalfHeight}px)`;
+
+  coordinates.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
   coordinates.textContent = `(${mouseX}px, ${mouseY}px)`;
-  coordinates.style.left = `${mouseX}px`;
-  coordinates.style.top = `${mouseY}px`;
-
-  horozontal.style.top = `${mouseY}px`;
-  vertical.style.left = `${mouseX}px`;
 }
 
 document.addEventListener("mousemove", moveImgage);
