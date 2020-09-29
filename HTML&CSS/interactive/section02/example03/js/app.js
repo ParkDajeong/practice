8c66af4a2fbd8b075010aeac62ac614f3da1309c
ParkDajeong <sobeast980@gmail.com>
2020-09-29 18:26:56 +0900

    주석 추가

--- HTML&CSS/interactive/section02/example03/js/app.js
+++ HTML&CSS/interactive/section02/example03/js/app.js
@@ -17,6 +17,7 @@ const loop = () => {
   mouseX += (x - mouseX) * speed;
   mouseY += (y - mouseY) * speed;
 
+  // cursorItem 은 mousemove용.
   cursorItem.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
 
   window.requestAnimationFrame(loop);
@@ -27,6 +28,7 @@ loop();
 window.addEventListener("mousemove", mouseFunc);
 
 // Yes Button In-Out
+// transform을 두번 사용해야 하기때문에, cursorItem 안의 circle은 확대/축소용으로 사용.
 btnYes.addEventListener("mouseover", () => {
   circle.style.transform = "scale(.3)";
 });
